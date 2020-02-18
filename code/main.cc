b707de6b493939039f7235148abcfd81aec98e4e
Philip Lindh <philip.lindh98@hotmail.com>
2020-02-18 18:17:52 +0100

    Code reformat + cpp hint

--- code/main.cc
+++ code/main.cc
@@ -1,31 +1,21 @@
-//------------------------------------------------------------------------------
-//  main.cc
-//  (C) 2020 Individual contributors, see AUTHORS file
-//------------------------------------------------------------------------------
 #include "stdneb.h"
 #include "foundation.h"
 #include "core/coreserver.h"
 #include "core/sysfunc.h"
-#include "exampleapp.h"
+#include "Engine.h"
 #include "system/appentry.h"
-
-using namespace Core;
 
 ImplementNebulaApplication()
 
-//------------------------------------------------------------------------------
-/**
-*/
-void
-NebulaMain(const Util::CommandLineArgs& args)
+void NebulaMain(const Util::CommandLineArgs& args)
 {
-	Example::ExampleApplication app;
-	app.SetCmdLineArgs(args);
+    GameEngine::Engine app;
+    app.SetCmdLineArgs(args);
     if (app.Open())
     {
         app.Run();
         app.Close();
     }
 
-	Core::SysFunc::Exit(0);
+    Core::SysFunc::Exit(0);
 }
